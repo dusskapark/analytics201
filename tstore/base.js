c971c86ce8c2c8530ef24632c077b705874b888d
dusskapark <dusskapark@gmail.com>
2015-10-08 15:15:50 +0900

    수정

--- tstore/base.js
+++ tstore/base.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-  url = "http://metadata.co.kr/tstore/data/sampleN.json";
+  url = "./tstore/data/sampleN.json";
   PlanetX.init({appkey : "a2121b2c-529a-31e1-9550-2606b1196ac3"});
 
   callApi = function( url, successFn ) {
@@ -48,14 +48,12 @@ randomString = function () {
         "searchKeyword": item.prodNm,
         "order": "R"},
         search_callback
-        // "success": search_callback,
-        // "fail": "console.log('fail')"
       );
 
       function search_callback( data ) {
 
         console.log(randomString() + data.tstore.totalCount);
-        console.log(data.tstore.products.product);
+        // console.log(data.tstore.products.product);
 
 
         }
