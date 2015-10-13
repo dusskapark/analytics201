195a1f512c768c6db6b8118224bd57f9ee421b3d
dusskapark <dusskapark@gmail.com>
2015-10-13 10:54:37 +0900

    코드 수정

--- js/init.js
+++ js/init.js
@@ -1,16 +1,26 @@
 $(document).ready(function() {
 
-// isotope 추가
-$('.grid').isotope({
-  // options
-  itemSelector: '.grid-item',
-  layoutMode: 'fitRows'
-});
+  // API 를 통해서 데이터를 불러온다.
+  // 식당이 오픈하지 않는 날은 분기처리한다. - 아직 하지 않은 일
+  makeApiCall = function (date, responseFn) {
+
+    var request = gapi.client.bobplanetApi.menuOfDate({
+      'date': date
+    });
+    request.then(function(resp) {
+
+      responseFn(resp.result);
+
+    }, function(reason) {
+
+      console.log('Error: ' + reason.result.error.message);
+
+    });
+  }
 
 
 
 doSomethingAfterLoading = function () {
-  // body...
 
   // 오늘 날짜를 가져온다.
   var today = new Date();
@@ -36,100 +46,137 @@ doSomethingAfterLoading = function () {
   });
 }
 
+function response_json(json) {
 
-  // API 를 통해서 데이터를 불러온다.
-  // 식당이 오픈하지 않는 날은 분기처리한다. - 아직 하지 않은 일
-  makeApiCall = function (date, responseFn) {
+  console.log(json);
+
+  // 상세 내용을 출력한다.
+  var dailyList = json.menu;
+  var $menuOfDate = "";
+  $.each(dailyList, function (i, v) {
+    // body...
+    $menuOfDate += "" +
+      "<div class=\"col s12 m6 l3\">" +
+        "<div class=\"card medium\">" +
+          "<div class=\"card-image waves-effect waves-block waves-light\">" +
+            "<img height='230px' src=\""+ v.item.iconURL +"\" class=\"activator\">" +
+            "<span class=\"card-title truncate\">"+ v.item.id +"</span>" +
+          "</div>" +
+          "<div class=\"card-content\">" +
+            "<span class='card-title activator grey-text text-darken-4 truncate'><i class=\"material-icons circle\">access_time</i>" + v.when + "</span>" +
+            "<p class='activator' alt='description'>뭘 넣어야 할지는 모르겠으니깐 일단은 아무거나 넣어두려고 합니다. 글자수가 많으면 어떻게 되는지도 궁금하네요 </p>" +  
+          "</div>" +
+          "<div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">매뉴 평가<i class=\"material-icons right close\">close</i></span>"+
+            "<div class='container center-align'>" +
+              "<i class=\"fa fa-heart\"></i> " +
+              "<i class=\"fa fa-heart\"></i> " +
+              "<i class=\"fa fa-heart\"></i> " +
+              "<i class=\"fa fa-heart-o\"></i> " +
+              "<i class=\"fa fa-heart-o\"></i> " +
+              "<p class='left-align' alt='description'>" + v.item.submenu + "</p>" +
+            "</div>" +
+          "</div>" +
+        "</div>" +
+      "</div>";
+    });
+
+    $days = "" +
+      "<li class=\"prev\" data-id='"+ json.previousDate +"'>" +
+        "<div class=\"collapsible-header\"><i class=\"material-icons\">filter_drama</i>"+ json.previousDate +"</div>" +
+        "<div class=\"collapsible-body\">"+
+          "<p>Lorem ipsum dolor sit amet.</p>"+
+        "</div>" +
+      "</li>" +
+      "<li data-id=\""+ json.date +"\">" +
+        "<div class=\"collapsible-header active\"><i class=\"material-icons\">place</i>"+ json.date +"</div>" +
+        "<div data-id=\"collapsible-body\">"+
+          "<div class=\"row\">"+
+          $menuOfDate +
+          "</div>" +
+        "</div>" +
+      "</li>" +
+      "<li class=\"next\" data-id='"+ json.nextDate +"'>" +
+        "<div class=\"collapsible-header\"><i class=\"material-icons\">whatshot</i>"+ json.nextDate +"</div>" +
+        "<div class=\"collapsible-body\">"+
+          "<p>Lorem ipsum dolor sit amet.</p>"+
+        "</div>" +
+      "</li>";
+
+      $("#bobcard").append($days);
+
+
+  $('.collapsible').collapsible({
+    accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  });
+
+};
+
+
+  $("body").on("click", ".prev", function ( e ) {
+    // body...
+    var clickedDate = $(this).attr('data-id');
+    // $(this).removeClass('prev');
+    makeApiCall(clickedDate, response_prev);
+  });
+
+
 
     var request = gapi.client.bobplanetApi.menuOfDate({
-      'date': date
+      'date': clickedDate
     });
     request.then(function(resp) {
-      responseFn(resp.result);
+      var dailyList = resp.result.menu;
+      console.log(dailyList);
+      // var $menuOfDate = "";
+      // $.each(dailyList, function (i, v) {
+      //   // body...
+      //   $menuOfDate = "" +
+      //     "<div class=\"col s12 m6 l3\">" +
+      //       "<div class=\"card medium\">" +
+
+      //         "<div class=\"card-image waves-effect waves-block waves-light\">" +
+      //           "<img height='230px' src=\""+ v.item.iconURL +"\" class=\"activator\">" +
+      //           "<span class=\"card-title truncate\">"+ v.item.id +"</span>" +
+      //         "</div>" +
+      //         "<div class=\"card-content\">" +
+      //           "<span class='card-title activator grey-text text-darken-4 truncate'><i class=\"material-icons circle\">access_time</i>" + v.when + "</span>" +
+      //           "<p class='activator' alt='description'>뭘 넣어야 할지는 모르겠으니깐 일단은 아무거나 넣어두려고 합니다. 글자수가 많으면 어떻게 되는지도 궁금하네요 </p>" +  
+      //         "</div>" +
+      //         "<div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">매뉴 평가<i class=\"material-icons right close\">close</i></span>"+
+      //           "<div class='container center-align'>" +
+      //             "<i class=\"fa fa-heart\"></i> " +
+      //             "<i class=\"fa fa-heart\"></i> " +
+      //             "<i class=\"fa fa-heart\"></i> " +
+      //             "<i class=\"fa fa-heart-o\"></i> " +
+      //             "<i class=\"fa fa-heart-o\"></i> " +
+      //             "<p class='left-align' alt='description'>" + v.item.submenu + "</p>" +
+      //           "</div>" +
+      //         "</div>" +
+
+      //       "</div>" +
+      //     "</div>";
+      //   $(this).find(".collapsible-body").append($menuOfDate);
+      //   });
+
+        // $(this).find("collapsible-body > p").remove()
+
+
+        $days = "" +
+          "<li class=\"prev\" data-id='"+ resp.result.previousDate +"'>" +
+            "<div class=\"collapsible-header\"><i class=\"material-icons\">filter_drama</i>"+ resp.result.previousDate +"</div>" +
+            "<div class=\"collapsible-body\">"+
+              "<p>Lorem ipsum dolor sit amet.</p>"+
+            "</div>" +
+          "</li>";
+
+        $(this).prepend($days);
+
     }, function(reason) {
       console.log('Error: ' + reason.result.error.message);
     });
-  }
+  });
 
 
-    $("body").on("click", "#bobcard > li", function ( e ) {
-      // body...
-      var clickedDate = $(this).attr('data-id');
-      makeApiCall(clickedDate, response_clickedDate);
-
-
-
-    });
-
-  function response_json(json) {
-
-    console.log(json);
-
-    $('.collection').find('.title').text(json.date);
-    $('#previousDate').attr("data-id", json.previousDate);
-    $('#nextDate').attr("data-id", json.nextDate);
-
-    // 상세 내용을 출력한다.
-    var dailyList = json.menu;
-    var $menuOfDate = "";
-    var $days;
-    $.each(dailyList, function (i, v) {
-      // body...
-      $menuOfDate += "" +
-        "<div class=\"col s12 m6 l3\">" +
-          "<div class=\"card medium\">" +
-            "<div class=\"card-image waves-effect waves-block waves-light\">" +
-              "<img height='230px' src=\""+ v.item.iconURL +"\" class=\"activator\">" +
-              "<span class=\"card-title truncate\">"+ v.item.id +"</span>" +
-            "</div>" +
-            "<div class=\"card-content\">" +
-              "<span class='card-title activator grey-text text-darken-4 truncate'><i class=\"material-icons circle\">access_time</i>" + v.when + "</span>" +
-              "<p class='activator' alt='description'>뭘 넣어야 할지는 모르겠으니깐 일단은 아무거나 넣어두려고 합니다. 글자수가 많으면 어떻게 되는지도 궁금하네요 </p>" +  
-            "</div>" +
-            "<div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">매뉴 평가<i class=\"material-icons right close\">close</i></span>"+
-              "<div class='container center-align'>" +
-                "<i class=\"fa fa-heart\"></i> " +
-                "<i class=\"fa fa-heart\"></i> " +
-                "<i class=\"fa fa-heart\"></i> " +
-                "<i class=\"fa fa-heart-o\"></i> " +
-                "<i class=\"fa fa-heart-o\"></i> " +
-                "<p class='left-align' alt='description'>" + v.item.submenu + "</p>" +
-              "</div>" +
-            "</div>" +
-          "</div>" +
-        "</div>";
-
-      });
-
-      $days = "" +
-        "<li data-id='"+ json.previousDate +"'>" +
-          "<div class=\"collapsible-header\"><i class=\"material-icons\">filter_drama</i>"+ json.previousDate +"</div>" +
-          "<div class=\"collapsible-body\"><p>Lorem ipsum dolor sit amet.</p></div>" +
-        "</li>" +
-        "<li data-id=\""+ json.date +"\" class=\"active\">" +
-          "<div class=\"collapsible-header active \">"+
-          "<i class=\"material-icons\">place</i>"+ json.date +
-          // "<span> ("+ json.menu.length +")</span>"+
-          "</div>" +
-          "<div data-id=\"collapsible-body\">"+
-            "<div class=\"row grid\">"+
-            $menuOfDate +
-            "</div>" +
-          "</div>" +
-
-        "</li>" +
-        "<li class='"+ json.nextDate +"'>" +
-          "<div class=\"collapsible-header\"><i class=\"material-icons\">whatshot</i>"+ json.nextDate +"</div>" +
-          "<div class=\"collapsible-body\"><p>Lorem ipsum dolor sit amet.</p></div>" +
-        "</li>";
-
-        $("#bobcard").append($days);
-
-    $('.collapsible').collapsible({
-      accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-    });
-
-  };
 
 
 });
